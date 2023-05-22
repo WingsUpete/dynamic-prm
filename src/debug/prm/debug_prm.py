588fd875934f3549c4eaf720a5f320868d3f3c6f
WingsUpete <petershen815@126.com>
2023-05-22 15:41:00 +0800

    [DEBUG] Uses sample data in the debug file

--- src/debug/prm/debug_prm.py
+++ src/debug/prm/debug_prm.py
@@ -1,8 +1,8 @@
 import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), '../../'))  # load core
-import random
 import time
+import pickle
 
 import logging
 
@@ -13,49 +13,23 @@ logger.info('Initializing...')
 import matplotlib.pyplot as plt
 
 from core.prm import *
-from core.util import *
-
-
-def get_test_problem():
-    cur_map_range = [0, 60]
-    cur_robot_radius = 2
-    cur_seed = None
-    # cur_seed = 666666
-    random.seed(cur_seed)
-    max_or = 6
-
-    # obstacles
-    o_dict = ObstacleDict(map_range=cur_map_range, robot_radius=cur_robot_radius)
-    for i in range(40):
-        cur_o = RoundObstacle(x=20.0, y=i, r=random.uniform(0, max_or), obstacle_type=ObstacleType.NORMAL)
-        o_dict.add_obstacle(obstacle=cur_o)
-    for i in range(40):
-        cur_o = RoundObstacle(x=40.0, y=60.0 - i, r=random.uniform(0, max_or), obstacle_type=ObstacleType.NORMAL)
-        o_dict.add_obstacle(obstacle=cur_o)
-
-    return {
-        'map_range': cur_map_range,
-        'obstacles': o_dict,
-        'robot_radius': cur_robot_radius,
-        # DEBUG
-        'rnd_seed': cur_seed,
-        # 'n_samples': 100
-    }, {
-        'start': [10, 10],
-        'goal': [50, 50],
-    }
 
 
 def test_prm(show_map=False, animation=True):
     logger.info('Start PRM program.')
 
-    # Define the problem
-    test_problem, test_query = get_test_problem()
+    # Load the sample problem
+    test_problem_folder = '../../sample_data/test_problem/'
+    with open(os.path.join(test_problem_folder, 'map.pickle'), 'rb') as f:
+        test_map = pickle.load(f)
+    with open(os.path.join(test_problem_folder, 'queries.pickle'), 'rb') as f:
+        test_queries = pickle.load(f)
+    test_query = test_queries[0]
 
     # Create the RRT solver
     logger.info('Initializing the PRM solver...')
     t0 = time.time()
-    my_prm = Prm(**test_problem)
+    my_prm = Prm(**test_map)
     logger.info('PRM setup uses %f sec.' % (time.time() - t0))
 
     # print map
